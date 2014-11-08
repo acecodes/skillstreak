74aaf57a8b46af4c55ce9ba3bd09e959f78471b1
Ace <ace.github@gmail.com>
2014-11-08 12:20:39 -0800

    Functioning database interaction added to front page

--- skillstreak.py
+++ skillstreak.py
@@ -66,9 +66,21 @@ def basics():
 # Views
 
 # Main page view
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+	form = NameForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username = form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
+		session['name'] = form.name.data
+		form.name.data = ''
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 # User view
 @app.route('/user/<name>')
