6ff9b7f6a1fb10563696bf8bfcc0eca3b9401a78
Ace <ace.github@gmail.com>
2014-12-26 15:02:33 -0800

    First steps towards login/signup

--- streaks/views.py
+++ streaks/views.py
@@ -1,9 +1,22 @@
 from django.views import generic
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 class IndexView(generic.View):
     template_name = 'index.html'
 
     def get(self, request):
     	return render(request, self.template_name)
+
+class MemberView(generic.View):
+	template_name = 'member.html'
+
+	def get(self, request):
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return render(request, self.template_name)
