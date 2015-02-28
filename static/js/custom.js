db6241f81384a100ef1756825fa6b2bce61857ff
Ace <ace.github@gmail.com>
2015-02-27 17:52:10 -0800

    Added fades for refresh, post and account options, implemented primitive error messsages

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,79 +1,84 @@
 $(document).ready(function() {
 
-        var refresh = function() {
-            $(".streaks").load(" .single_streak");
-        };
+    // Fade number to be recycled
+    var fade = 650;
+
+    // Refresh function for all operations
+    var refresh = function() {
+        $(".streaks").load(" .single_streak").hide().fadeIn(fade);
+    };
+
+    // Show account options
+    $("#account_options").click(function() {
+        $("#dash_options").fadeIn(fade);
+    });
+
+    // Hide account options
+    $("#account_options_hide").click(function () {
+        $("#dash_options").fadeOut(fade);
+    });
 
 
-            // Fade in effect for streak boxes
-            $(".streak_box").hide().fadeIn(450);
+    // Fade in effect for streak boxes
+    $(".streak_box").hide().fadeIn(fade);
 
-            var csrftoken = $.cookie('csrftoken');
 
-            function csrfSafeMethod(method) {
-                // these HTTP methods do not require CSRF protection
-                return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    // CSRF cookie
+    var csrftoken = $.cookie('csrftoken');
+
+
+    // CSRF function that allows AJAX to make API queries
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
             }
-            $.ajaxSetup({
-                beforeSend: function(xhr, settings) {
-                    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-                    }
-                }
-            });
+        }
+    });
 
-            // Button to test AJAX posting
-            $(".post_button").click(function() {
+    // Button to test AJAX posting
+    $(".post_button").click(function() {
 
-                $.ajax({
-                    type: "POST",
-                    url: '/api/streaks/',
-                    contentType: 'application/json',
-                    dataType: 'json',
-                    data: JSON.stringify({
-                        "user": "root",
-                        "activity": "AJAX17 testing",
-                        "start": "2015-02-21T22:53:51Z",
-                        "current_streak": 2,
-                        "longest_streak": 5,
-                        "resets": 0,
-                        "last_reset": "2015-02-25T22:53:55Z",
-                        "notes": "A",
-                        "csrfmiddlewaretoken": csrftoken
-                    })
+        $.ajax({
+            type: "POST",
+            url: '/api/streaks/',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify({
+                "user": "root",
+                "activity": "AJAX1 testing",
+                "start": "2015-02-21T22:53:51Z",
+                "current_streak": 2,
+                "longest_streak": 5,
+                "resets": 0,
+                "last_reset": "2015-02-25T22:53:55Z",
+                "notes": "A",
+                "csrfmiddlewaretoken": csrftoken
+            })
 
-                }).error(function(r) {
-                    console.log(r);
-                })
-                    .success(function(r) {
-                        refresh();
-                        console.log("success", r);
-                    });
-            });
-
-            $(".delete_button").click(function() {
-                var id = "{{ i.id }}";
-                $.ajax({
-                    type: "DELETE",
-                    url: '/api/streaks/' + id,
-                    contentType: 'application/json',
-                    dataType: 'json',
-                    data: JSON.stringify({
-                        "id": 31,
-                        "csrfmiddlewaretoken": csrftoken
-                    })
-
-                }).error(function(r) {
-                    console.log(r);
-                })
-                    .success(function(r) {
-                        refresh();
-                        console.log("success", r);
-                    });
-            });
-
-            // Button to test AJAX refresh
-            $(".refresh_button").click(function() {
+        }).error(function(r) {
+            console.log(r);
+            $("#error").append(JSON.stringify(r)).fadeIn(fade);
+        })
+            .success(function(r) {
                 refresh();
+                console.log("success", r);
             });
-        });
+    });
+
+
+    // Button for deleting entries
+    $(".delete_button").click(function() {
+        // TO-DO: Create delete button
+        console.log("Nothing here yet!");
+    });
+
+    // Button to test AJAX refresh
+    $(".refresh_button").click(function() {
+        refresh();
+    });
+});
