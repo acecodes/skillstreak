0a2ce1073820eff22b4ff9f8686b70c5027eb93e
Ace <ace.github@gmail.com>
2015-03-01 13:41:18 -0800

    Restructured settings files

---
+++ skillstreak/settings/dev.py
@@ -0,0 +1,47 @@
+"""
+Django settings for skillstreak project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from skillstreak.common import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'J2vv1*$)(z$^=2%pjp!gr<!@1tm@npZb+8tc?_7#wds&+(jql3)'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'skillstreak',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+    }
+}
+
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Allow for email backend testing - change for production!
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'test_messages')
